6f579d3cccd535e0d17a94f1e7f1240e8d124493
nasum <tomato.wonder.life@gmail.com>
2017-03-07 21:38:18 +1000

    hello world

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // initial target dom
 var element = document.createElement('div');
 element.id = "app";
 document.body.appendChild(element)
+
+const element = <h1>hello world</h1>
+
+ReactDOM.render(
+  element,
+  document.getElementById('app')
+);
