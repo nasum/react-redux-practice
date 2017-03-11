9afb866fb124d1c31d7d9042411d39ca848aa8f3
nasum <tomato.wonder.life@gmail.com>
2017-03-12 00:14:49 +0900

    rename function name

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-var gulpRename = require('gulp-rename');
-var gulpReplace = require('gulp-replace');
+var gRename = require('gulp-rename');
+var gReplace = require('gulp-replace');
 var minimist = require('minimist');
 
 var args = minimist(process.argv.slice(2));
@@ -8,8 +8,8 @@ var args = minimist(process.argv.slice(2));
 gulp.task('create:component', function(){
   gulp
     .src('./src/templates/component_template.jsx')
-    .pipe(gulpRename(args.name + '.jsx'))
-    .pipe(gulpReplace(/ComponentTemplate/g, args.name))
+    .pipe(gRename(args.name + '.jsx'))
+    .pipe(gReplace(/ComponentTemplate/g, args.name))
     .pipe(gulp.dest('./src/components'))
   console.log(args)
 });
